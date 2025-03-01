56ed5e6b4047d537358abec1371bf03a0a25b151
ilcherry <1062217965@qq.com>
2025-03-01 15:30:07 +0800

    更新 Profile.vue，添加居住地信息以丰富个人资料展示

--- docs/.vuepress/components/Profile.vue
+++ docs/.vuepress/components/Profile.vue
@@ -2,7 +2,9 @@
   <div class="profile">
     <h1>高威</h1>
     <div>
-      <span>31 岁</span>|<span>全日制本科</span>|<span>电话:15623412869</span>|<span>邮箱:1062217965@qq.com</span>
+      <span>31 岁</span>|<span>全日制本科</span>|<span>武汉</span>|<span>电话:15623412869</span>|<span
+        >邮箱:1062217965@qq.com</span
+      >
     </div>
     <div>
       <span>7 年半工作经验</span>|<span>求职意向：<strong>全职前端开发</strong></span>
